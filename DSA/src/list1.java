76356e7c8832e775de9984a30df4d98bf6a38afe
Ningombam Borish Singh <131790729+BORISH1@users.noreply.github.com>
2025-12-20 00:44:12 +0530

    20 december 2025

---
+++ DSA/src/list1.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class list1 {
+    public static void main(String args[])
+    {
+        int[] arr = new int[]{10, 20, 30, 40};
+        System.out.println(Arrays.toString(arr));
+    }
+}
